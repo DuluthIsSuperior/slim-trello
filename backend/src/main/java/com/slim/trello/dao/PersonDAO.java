714a613de78f6b0b576e4b5c452b550fc5395c0f
Kyle <kyle.i.dick@outlook.com>
2020-12-09 14:29:18 -0500

    Added PersonDAO and added CORS header to PersonController

--- backend/src/main/java/com/slim/trello/dao/PersonDAO.java
+++ backend/src/main/java/com/slim/trello/dao/PersonDAO.java
@@ -1,7 +1,7 @@
 package com.slim.trello.dao;
 
+import com.slim.trello.entity.Person;
 import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,7 +10,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-public class PersonDAO implements MyDAO {
+public class PersonDAO implements MyDAO<Person> {
     private final EntityManager entityManager;
 
     @Autowired
@@ -18,11 +18,56 @@ public class PersonDAO implements MyDAO {
         this.entityManager = entityManager;
     }
 
+    private Session getSession() {
+        return entityManager.unwrap(Session.class);
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
     @Transactional
-    public List<Object> findAll() {
-        Session session = entityManager.unwrap(Session.class);
-        Query<Object> myQuery = session.createQuery("from Person");
-        return myQuery.getResultList();
+    public List<Person> findAll() {
+        return getSession().createQuery("from Person").getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Person findByID(int ID) {
+        return getSession().get(Person.class, ID);
+    }
+
+    @Override
+    @Transactional
+    public boolean update(Person person) {
+        try {
+            getSession().saveOrUpdate(person);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    @Transactional
+    public boolean save(Person person) {
+        person.setId(0);
+        return update(person);
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteByID(int ID) {
+        Person p = findByID(ID);
+        if (p == null) {
+            System.out.println("Person not found");
+            return false;
+        }
+        try {
+            getSession().delete(p);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
