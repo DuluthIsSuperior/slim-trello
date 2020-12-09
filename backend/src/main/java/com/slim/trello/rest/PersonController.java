427ce39ffa11db9f7399116eac2b08b68ef069ae
Tyrese Merrick <merricktyrese@gmail.com>
2020-12-09 14:06:39 -0600

    Controllers

--- backend/src/main/java/com/slim/trello/rest/PersonController.java
+++ backend/src/main/java/com/slim/trello/rest/PersonController.java
@@ -1,19 +1,20 @@
 package com.slim.trello.rest;
 
 import com.slim.trello.dao.MyDAO;
+import com.slim.trello.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 public class PersonController {
     private final MyDAO myDAO;
 
     @Autowired
-    public PersonController(@Qualifier("personDAO") MyDAO myDAO) {
+    public PersonController(@Qualifier("personIMPL") MyDAO myDAO) {
         this.myDAO = myDAO;
     }
 
@@ -21,4 +22,29 @@ public class PersonController {
     public List<Object> findAll() {
         return myDAO.findAll();
     }
+
+    @PostMapping("/addPerson")
+    public Person addPerson(@RequestBody Person thePerson){
+        thePerson.setId(0);
+        myDAO.save(thePerson);
+    }
+    @PutMapping("/updatePerson")
+    public Person updatePerson(@RequestBody Person updatePerson) {
+        myDAO.save(updatePerson);
+        return updatePerson;
+    }
+    @DeleteMapping("/deletePerson/{personId}")
+    public String deletePerson(@PathVariable int personId) {
+        Person tempPerson = (Person) myDAO.findById(personId);
+
+        //This will throw an exception if the employee is null
+        if(tempPerson == null) {
+            throw new RuntimeException("Person is not found : " + personId);
+        }
+
+        //This will execute the deleteByID.
+        myDAO.deleteById(personId);
+        return "Deleted person id : " + personId;
+    }
+
 }
