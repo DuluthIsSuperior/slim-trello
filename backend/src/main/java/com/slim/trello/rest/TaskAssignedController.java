71d548366baf9180408897619f267f3833f5ecde
Tyrese Merrick <merricktyrese@gmail.com>
2020-12-10 14:39:28 -0600

    updated controller

--- backend/src/main/java/com/slim/trello/rest/TaskAssignedController.java
+++ backend/src/main/java/com/slim/trello/rest/TaskAssignedController.java
@@ -1,6 +1,8 @@
 package com.slim.trello.rest;
 
 import com.slim.trello.dao.MyDAO;
+import com.slim.trello.entity.Person;
+import com.slim.trello.entity.Task;
 import com.slim.trello.entity.TaskAssigned;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,40 +10,70 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
 public class TaskAssignedController {
-    private final MyDAO myDAO;
+    private final MyDAO<TaskAssigned> myDAO;
+    private final MyDAO<Person> personMyDAO;
+    private final MyDAO<Task> taskMyDAO;
 
     @Autowired
-    public TaskAssignedController(@Qualifier("taskIMPL") MyDAO myDAO) {
+    public TaskAssignedController(@Qualifier("taskAssignedDAO") MyDAO<TaskAssigned> myDAO, @Qualifier("personDAO") MyDAO<Person> personMyDAO,
+                                  @Qualifier("taskDAO")MyDAO<Task> taskMyDAO) {
         this.myDAO = myDAO;
+        this.personMyDAO = personMyDAO;
+        this.taskMyDAO = taskMyDAO;
     }
 
     @GetMapping("/retrieveAllTaskAssigned")
-    public List<Object> findAll() {
+    public List<TaskAssigned> findAll() {
         return myDAO.findAll();
     }
 
     @PostMapping("/addTaskAssigned")
     public TaskAssigned addTaskAssigned(@RequestBody TaskAssigned theTaskAssigned){
         theTaskAssigned.setId(0);
-        myDAO.save(theTaskAssigned);
+        return myDAO.save(theTaskAssigned);
     }
+
+    @PostMapping("/addTaskAssigned")
+    public String addTaskAssigned(@RequestBody TaskAssigned theTaskAssigned,int personId,
+                                         int taskId){
+        Task tempTask = taskMyDAO.findByID(taskId);
+        Person tempPerson = personMyDAO.findByID(personId);
+        theTaskAssigned.setId(0);
+
+        //This will throw an exception if the employee is null
+        if(tempTask == null || tempPerson == null) {
+            return "Not found";
+        }
+        else{
+             myDAO.save(theTaskAssigned);
+             return "added";
+        }
+
+
+    }
+
+
     @PutMapping("/updateTaskAssigned")
     public TaskAssigned updateTaskAssigned(@RequestBody TaskAssigned updateTaskAssigned) {
         myDAO.save(updateTaskAssigned);
         return updateTaskAssigned;
     }
+
+
     @DeleteMapping("/deleteTaskAssigned/{personId}")
     public String deleteTaskAssigned(@PathVariable int personId) {
-        TaskAssigned tempTaskAssigned = (TaskAssigned) myDAO.findById(personId);
+        TaskAssigned tempTaskAssigned = myDAO.findByID(personId);
 
         //This will throw an exception if the employee is null
         if(tempTaskAssigned == null) {
-            throw new RuntimeException("TaskAssigned is not found : " + personId);
+            return "Not found";
         }
 
         //This will execute the deleteByID.
-        myDAO.deleteById(personId);
+        myDAO.deleteByID(personId);
         return "Deleted person id : " + personId;
-    }
+    } 
 }
